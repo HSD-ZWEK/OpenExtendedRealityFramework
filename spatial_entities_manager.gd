1081bf522f3244dc066ea181e8e3d6d7856ca70f
ntluk <niklas.tluk@study.hs-dusseldorf.de>
2026-07-10 14:42:14 +0200

    spatial entities manager

---
+++ spatial_entities_manager.gd
@@ -0,0 +1,203 @@
+class_name SpatialEntitiesManager
+extends Node3D
+
+## Signals a new spatial entity node was added.
+signal added_spatial_entity(node: XRNode3D)
+
+## Signals a spatial entity node is about to be removed.
+signal removed_spatial_entity(node: XRNode3D)
+
+## Scene to instantiate for spatial anchor entities.
+@export var spatial_anchor_scene: PackedScene
+
+## Scene to instantiate for plane tracking spatial entities.
+@export var plane_tracker_scene: PackedScene
+
+## Scene to instantiate for marker tracking spatial entities.
+@export var marker_tracker_scene: PackedScene
+
+# Trackers we manage nodes for.
+var _managed_nodes: Dictionary[XRTracker, XRAnchor3D]
+
+# Enter tree is called whenever our node is added to our scene.
+func _enter_tree():
+	# Connect to signals that inform us about tracker changes.
+	XRServer.tracker_added.connect(_on_tracker_added)
+	XRServer.tracker_updated.connect(_on_tracker_updated)
+	XRServer.tracker_removed.connect(_on_tracker_removed)
+
+	# Set up existing trackers.
+	var trackers : Dictionary = XRServer.get_trackers(XRServer.TRACKER_ANCHOR)
+	for tracker_name in trackers:
+		var tracker: XRTracker = trackers[tracker_name]
+		if tracker and tracker is OpenXRSpatialEntityTracker:
+			_add_tracker(tracker)
+
+
+# Exit tree is called whenever our node is removed from our scene.
+func _exit_tree():
+	# Clean up our signals.
+	XRServer.tracker_added.disconnect(_on_tracker_added)
+	XRServer.tracker_updated.disconnect(_on_tracker_updated)
+	XRServer.tracker_removed.disconnect(_on_tracker_removed)
+
+	# Clean up trackers.
+	for tracker in _managed_nodes:
+		removed_spatial_entity.emit(_managed_nodes[tracker])
+		remove_child(_managed_nodes[tracker])
+		_managed_nodes[tracker].queue_free()
+
+	_managed_nodes.clear()
+
+
+# See if this tracker should be managed by us and add it.
+func _add_tracker(tracker: OpenXRSpatialEntityTracker):
+	var new_node: XRAnchor3D
+
+	if _managed_nodes.has(tracker):
+		# Already being managed by us!
+		return
+
+	if tracker is OpenXRAnchorTracker:
+		# Note: Generally spatial anchors are controlled by the developer and
+		# are unlikely to be handled by our manager.
+		# But just for completeness we'll add it in.
+		if spatial_anchor_scene:
+			var new_scene = spatial_anchor_scene.instantiate()
+			if new_scene is XRAnchor3D:
+				new_node = new_scene
+			else:
+				push_error("Spatial anchor scene doesn't have an XRAnchor3D as a root node and can't be used!")
+				new_scene.free()
+	elif tracker is OpenXRPlaneTracker:
+		if plane_tracker_scene:
+			var new_scene = plane_tracker_scene.instantiate()
+			if new_scene is XRAnchor3D:
+				new_node = new_scene
+			else:
+				push_error("Plane tracking scene doesn't have an XRAnchor3D as a root node and can't be used!")
+				new_scene.free()
+	elif tracker is OpenXRMarkerTracker:
+		if marker_tracker_scene:
+			var new_scene = marker_tracker_scene.instantiate()
+			if new_scene is XRAnchor3D:
+				new_node = new_scene
+			else:
+				push_error("Marker tracking scene doesn't have an XRAnchor3D as a root node and can't be used!")
+				new_scene.free()
+	else:
+		# Type of spatial entity tracker we're not supporting?
+		push_warning("OpenXR Spatial Entities: Unsupported anchor tracker " + tracker.get_name() + " of type " + tracker.get_class())
+
+	if not new_node:
+		# No scene defined or able to be instantiated? We're done!
+		return
+
+	# Set up and add to our scene.
+	new_node.tracker = tracker.name
+	new_node.pose = "default"
+	_managed_nodes[tracker] = new_node
+	add_child(new_node)
+
+	added_spatial_entity.emit(new_node)
+
+
+# A new tracker was added to our XRServer.
+func _on_tracker_added(tracker_name: StringName, type: int):
+	if type == XRServer.TRACKER_ANCHOR:
+		var tracker: XRTracker = XRServer.get_tracker(tracker_name)
+		if tracker and tracker is OpenXRSpatialEntityTracker:
+			_add_tracker(tracker)
+
+
+# A tracked managed by XRServer was changed.
+func _on_tracker_updated(_tracker_name: StringName, _type: int):
+	# For now we ignore this, there aren't any changes here we need to react
+	# to and the instanced scene can react to this itself if needed.
+	pass
+
+
+# A tracker was removed from our XRServer.
+func _on_tracker_removed(tracker_name: StringName, type: int):
+	if type == XRServer.TRACKER_ANCHOR:
+		var tracker: XRTracker = XRServer.get_tracker(tracker_name)
+		if _managed_nodes.has(tracker):
+			# We emit this right before we remove it!
+			removed_spatial_entity.emit(_managed_nodes[tracker])
+
+			# Remove the node.
+			remove_child(_managed_nodes[tracker])
+
+			# Queue free the node.
+			_managed_nodes[tracker].queue_free()
+
+			# And remove from our managed nodes.
+			_managed_nodes.erase(tracker)
+			
+
+## Create a new spatial anchor with the associated child scene.
+## If persistent anchors are supported, this will be created as a persistent node
+## and we will store the child scene path with the anchor's UUID for future recreation.
+func create_spatial_anchor(p_transform: Transform3D, p_child_scene_path: String):
+	# Do we have anchor support?
+	if not OpenXRSpatialAnchorCapability.is_spatial_anchor_supported():
+		push_error("Spatial anchors are not supported on this device!")
+		return
+
+	# Adjust our transform to local space.
+	var t: Transform3D = global_transform.inverse() * p_transform
+
+	# Create anchor on our current manager.
+	var new_anchor = OpenXRSpatialAnchorCapability.create_new_anchor(t, RID())
+	if not new_anchor:
+		push_error("Couldn't create an anchor for %s." % [ p_child_scene_path ])
+		return
+
+	# Creating a new anchor should have resulted in an XRAnchor being added to the scene
+	# by our manager. We can thus continue assuming this has happened.
+
+	var anchor_scene = get_tracked_scene(new_anchor)
+	if not anchor_scene:
+		push_error("Couldn't locate anchor scene for %s, has the manager been configured with an applicable anchor scene?" % [ new_anchor.name ])
+		return
+	if not anchor_scene is OpenXRSpatialAnchor3D:
+		push_error("Anchor scene for %s is not an OpenXRSpatialAnchor3D scene, has the manager been configured with an applicable anchor scene?" % [ new_anchor.name ])
+		return
+
+	anchor_scene.set_child_scene(p_child_scene_path)
+
+
+## Removes this spatial anchor from our scene.
+## If the spatial anchor is persistent, the associated UUID will be cleared.
+func remove_spatial_anchor(p_anchor: XRAnchor3D):
+	# Do we have anchor support?
+	if not OpenXRSpatialAnchorCapability.is_spatial_anchor_supported():
+		push_error("Spatial anchors are not supported on this device!")
+		return
+
+	var tracker: XRTracker = XRServer.get_tracker(p_anchor.tracker)
+	if tracker and tracker is OpenXRAnchorTracker:
+		var anchor_tracker: OpenXRAnchorTracker = tracker
+		if anchor_tracker.has_uuid() and OpenXRSpatialAnchorCapability.is_spatial_persistence_supported():
+			# If we have a UUID we should first make the anchor unpersistent
+			# and then remove it on its callback.
+			remove_uuid(anchor_tracker.uuid)
+			OpenXRSpatialAnchorCapability.unpersist_anchor(anchor_tracker, RID(), _on_unpersist_complete)
+		else:
+			# Otherwise we can just remove it.
+			# This will remove it from the XRServer, which in turn will trigger cleaning up our node.
+			OpenXRSpatialAnchorCapability.remove_anchor(tracker)
+
+
+func _on_unpersist_complete(p_tracker: XRTracker):
+	# Our tracker is now no longer persistent, we can remove it.
+	OpenXRSpatialAnchorCapability.remove_anchor(p_tracker)
+
+
+## Retrieve the scene we've added for a given tracker (if any).
+func get_tracked_scene(p_tracker: XRTracker) -> XRNode3D:
+	for node in get_children():
+		if node is XRNode3D and node.tracker == p_tracker.name:
+			return node
+
+	return null
